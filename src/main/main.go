25e6563a36b7593066117702ad0827b13e04cf44
alberto-pelosi <hc1albert1ch@gmail.com>
2019-04-26 23:46:34 +0200

    indentation

--- src/main/main.go
+++ src/main/main.go
@@ -1,64 +1,62 @@
 package main
 
 import (
-    "fmt"
-    "math"
-    "math/rand"
-    "time"
+	"fmt"
+	"math"
+	"math/rand"
+	"time"
 )
 
 var packageVariable bool
 
 func main() {
-    rand.Seed(time.Now().UnixNano())
-    fmt.Println("My favorite number is", rand.Intn(1000))
-    sq()
-    pi()
-    fmt.Printf("add %d \n", add(1,3))
-    fmt.Printf("add2 %d \n", add2(3,5))
-    add3(7, 9)
-    a, b := swapz("ciao", "mondo")
-    fmt.Println(a, b)
-    c, d := nakedReturn(5)
-    fmt.Println("nakedReturn", c, d)
-    variableDefinition()
-    
-    
+	rand.Seed(time.Now().UnixNano())
+	fmt.Println("My favorite number is", rand.Intn(1000))
+	sq()
+	pi()
+	fmt.Printf("add %d \n", add(1, 3))
+	fmt.Printf("add2 %d \n", add2(3, 5))
+	add3(7, 9)
+	a, b := swapz("ciao", "mondo")
+	fmt.Println(a, b)
+	c, d := nakedReturn(5)
+	fmt.Println("nakedReturn", c, d)
+	variableDefinition()
+
 }
 
 func sq() {
-    fmt.Printf("Now you have %g problems.\n", math.Sqrt(7))
+	fmt.Printf("Now you have %g problems.\n", math.Sqrt(7))
 }
 
 func pi() {
-    fmt.Println(math.Pi)
+	fmt.Println(math.Pi)
 }
 
 func add(x int, y int) int {
-    return x + y
+	return x + y
 }
 
 func add2(x, y int) int {
-    return x + y
+	return x + y
 }
 
 func add3(x, y int) {
-    fmt.Printf("add3 %d", x + y)
-   
+	fmt.Printf("add3 %d", x+y)
+
 }
 
 func swapz(x, y string) (string, string) {
-    return y, x
+	return y, x
 }
 
 func nakedReturn(sum int) (x, y int) {
-    x = sum * 4 / 9
-    y = sum -x
-    return
+	x = sum * 4 / 9
+	y = sum - x
+	return
 }
 
 func variableDefinition() {
 	var funcVariable bool
 	fmt.Println(packageVariable, funcVariable)
 }
-
