74847beb8ac2b8080e427c38688e19017654377a
alberto-pelosi <hc1albert1ch@gmail.com>
2019-04-28 18:15:13 +0200

    for loop variants

--- src/otherpack/otherpack-otherfile.go
+++ src/otherpack/otherpack-otherfile.go
@@ -10,11 +10,31 @@ func Extpackfunc() {
 
 func Extpackcaller() {
 	forloop()
+	forloop2()
+	forloop3()
 }
 
 func forloop() {
 	sum := 0
 	for i := 0; i < 10; i++ {
+		sum += 1
+	}
+	fmt.Println(sum)
+}
+
+//init and post statements are optional
+func forloop2() {
+	sum := 1
+	for sum < 100 {
+		sum += 1
+	}
+	fmt.Println(sum)
+}
+
+// for is like while
+func forloop3() {
+	sum := 1
+	for sum < 100 {
 		sum += 1
 	}
 	fmt.Println(sum)
